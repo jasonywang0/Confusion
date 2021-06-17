b11e80cf4dfa457f8748e00f77a3419bbd1b6ebd
jasonywang0 <jasonywang0@gmail.com>
2021-06-17 13:06:14 -0700

    Single Page Applications Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -39,13 +39,24 @@ class Main extends Component {
         )
     }
 
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+
+
+
+
+
     return (
       <div>
         <Header />
         <Switch>
             <Route path='./home' component={HomePage} />
             <Route exact path ='/contact' component={Contact} />
-            <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />} />
+            <Route path='/menu/:dishId' component={DishWithId} />
             <Redirect to='/home'/>
         </Switch>
         <Footer />
