78fa790989dbab72ebd6617c090ea1352553ba83
jasonywang0 <jasonywang0@gmail.com>
2021-06-16 23:28:56 -0700

    React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import Main from './components/MainComponent';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 import {Component} from 'react';
 
 class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                <Main />
-            </div>
+            <BrowserRouter>
+                <div className="App">
+                    <Main />
+                </div>
+            </BrowserRouter>
         );
     }
 }
